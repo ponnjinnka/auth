e481b347827eaddf188ff753f8aa9f88d02f92ab
ponnjinnka <penponnnnjinnka@gmail.com>
2017-07-06 00:41:52 +0900

    完成

--- src/app.js
+++ src/app.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
-    apiKey: 'AIzaSyAz4GaFs61ZA5cTrjZj_GCOY_YovXz7nZo',
-    authDomain: 'auth-a7d96.firebaseapp.com',
-    databaseURL: 'https://auth-a7d96.firebaseio.com',
-    projectId: 'auth-a7d96',
-    storageBucket: 'auth-a7d96.appspot.com',
-    messagingSenderId: '265683504425'
+      apiKey: 'AIzaSyAz4GaFs61ZA5cTrjZj_GCOY_YovXz7nZo',
+      authDomain: 'auth-a7d96.firebaseapp.com',
+      databaseURL: 'https://auth-a7d96.firebaseio.com',
+      projectId: 'auth-a7d96',
+      storageBucket: 'auth-a7d96.appspot.com',
+      messagingSenderId: '265683504425'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
